313bf3327c8d399bfe1e20a412ca3fcdf677d3f9
Acer <a-krickiy@mail.ru>
2025-07-02 23:59:07 +0300

    2. Сканирование проекта [#1070]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -8,9 +8,7 @@ public class SpringDI {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(Store.class);
-        context.register(ConsoleInput.class);
-        context.register(StartUI.class);
+        context.scan("ru.job4j.di");
         context.refresh();
         StartUI ui = context.getBean(StartUI.class);
         IntStream.range(0, 2).forEach(i -> ui.add("Введите имя:"));
